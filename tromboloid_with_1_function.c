f160b352573171717158a9704223f72933c779d1
ENG19CS0226PranavDBhat <76091392+ENG19CS0226PranavDBhat@users.noreply.github.com>
2021-02-08 09:48:22 +0530

    Volume of tromboloid 1.0
    the volume of tramboloid activity in one function.

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -1,1 +1,9 @@
 //Write a program to find the volume of a tromboloid using one function
+#include<stdio.h>
+int main(){
+float h, b, d;
+printf("Enter the values: ");
+scanf("%f %f %f", &h, &b, &d);
+
+printf("The volume is = %f", ((h * d * b)+(d / b))/3);
+}
