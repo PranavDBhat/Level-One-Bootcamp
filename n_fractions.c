901526fcb18feadf86fbed5179fa74276f3fc128
ENG19CS0226PranavDBhat <76091392+ENG19CS0226PranavDBhat@users.noreply.github.com>
2021-02-18 10:53:12 +0530

    n_fractions.c 1.2
    activity 10

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,37 @@
 //WAP to find the sum of n fractions.
+#include <stdio.h>
+struct fraction{
+int num[100];
+int den[100];
+}fra;
+
+int fracSum(int n, int nu[],int de[]);
+int gcd(int a, int b);
+int lcm(int arr[], int n);
+
+int main(){
+int n;
+printf("Enter the number of fractions: ");
+scanf("%d", &n);
+for(int i=0;i<n;i++){
+printf("Enter numerator: ");
+scanf("%d", &fra.num[i]); 
+printf("Enter denominator: ");
+scanf("%d", &fra.den[i]);
+}
+fracSum(n, fra.num, fra.den);
+}
+
+int gcd(int a, int b){
+if (b == 0)
+return a;
+return gcd(b, a % b);
+}
+ 
+
+int lcm(int arr[], int n){
+int lcm = arr[0];
+for (int i = 1; i < n; i++)
+lcm = (((arr[i] * lcm)) / (gcd(arr[i], lcm)));
+return lcm;
+}
