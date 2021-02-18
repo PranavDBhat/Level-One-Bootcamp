cfcb44083a62e5a9d6e93aaa5d8dadc760b0677d
ENG19CS0226PranavDBhat <76091392+ENG19CS0226PranavDBhat@users.noreply.github.com>
2021-02-18 19:23:05 +0530

    two_fractions.c 1.3
    indented

--- two_fractions.c
+++ two_fractions.c
@@ -1,46 +1,48 @@
 //WAP to find the sum of two fractions.
 #include<stdio.h>
+
 struct fraction{
-int n1, n2;
-int d1, d2;
+    int num;
+    int den;
 };
+
 int denLcm(int deno1, int deno2);
-int addFra(struct fraction frac);
+int addFra(struct fraction frac1, struct fraction frac2);
 
 int main(){
-struct fraction fra;
-printf("Enter the fraction values n1 / d1 & n2/d2: ");
-scanf("%d %d %d %d", &fra.n1, &fra.d1, &fra.n2, &fra.d2);
-addFra(fra);
+    struct fraction fra1, fra2;
+    printf("Enter the fraction values n1 / d1 & n2/d2: ");
+    scanf("%d %d %d %d", &fra1.num, &fra1.den, &fra2.num, &fra2.den);
+    addFra(fra1, fra2);
 }
 
 int denLcm(int deno1, int deno2){
-int max;
+    int max;
 
-if(deno1 == deno2){
-max = deno1;
-return max;
-}
-else if(deno1 > deno2 ){
-max = deno1;
-}
-else{
-max = deno2;
-}
+    if(deno1 == deno2){
+        max = deno1;
+        return max;
+    }
+    else if(deno1 > deno2 ){
+        max = deno1;
+    }
+    else{
+        max = deno2;
+    }
 
 while(1){
-if(max % deno1 == 0 && max % deno2 == 0){
-return max;
-}
-max++;
-}
+    if(max % deno1 == 0 && max % deno2 == 0){
+        return max;
+    }
+    max++;
+    }
 }
 
-int addFra(struct fraction frac){
-int lcm, num1,num2,num3;
-lcm = denLcm( frac.d1, frac.d2);
-num1 = frac.n1 * (lcm / frac.d1);
-num2 = frac.n2 * (lcm / frac.d2);
-num3 = num1 + num2;
-printf("\nThe Sum of the fractions are = %d / %d", num3, lcm);
+int addFra(struct fraction frac1, struct fraction frac2){
+    int lcm, num1,num2,num3;
+    lcm = denLcm( frac1.den, frac2.den);
+    num1 = frac1.num * (lcm / frac1.den);
+    num2 = frac2.num * (lcm / frac2.den);
+    num3 = num1 + num2;
+    printf("\nThe Sum of the fractions are = %d / %d", num3, lcm);
 }
